c3eea6dfb58dd098bf639c65bacedfcbc98352b3
Mridula Shanker <Mridula Shanker@LAPTOP-T2PHJ0FM>
2018-10-07 16:52:21 -0400

    10/7/18

---
+++ Dice/src/IfElse.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ * @author Mridula Shanker
+ *
+ */
+public class IfElse 
+{
+
+	public static void main(String[] args) 
+	{
+
+				int count = 0;
+				Dice dice1 = new Dice();
+				
+				int roll = dice1.roll();
+				System.out.println(roll);
+				if (roll == 3)
+					{
+					count++;
+					}
+				roll = dice1.roll();
+				System.out.println(roll);
+				if (roll == 3)
+					{
+					count++;
+					}
+				roll = dice1.roll();
+				System.out.println(roll);
+				if (roll == 3)
+					{
+					count++;
+					}
+				roll = dice1.roll();
+				System.out.println(roll);
+				if (roll == 3)
+					{
+					count++;
+					}
+				roll = dice1.roll();
+				System.out.println(roll);
+				if (roll == 3)
+					{
+					count++;
+					}
+				roll = dice1.roll();
+				System.out.println(roll);
+				if (roll == 3)
+					{
+					count++;
+					}
+				roll = dice1.roll();
+				System.out.println(roll);
+				if (roll == 3)
+					{
+					count++;
+					}
+				roll = dice1.roll();
+				System.out.println(roll);
+				if (roll == 3)
+					{
+					count++;
+					}
+				roll = dice1.roll();
+				System.out.println(roll);
+				if (roll == 3)
+					{
+					count++;
+					}
+				roll = dice1.roll();
+				System.out.println(roll);
+				if (roll == 3)
+					{
+					count++;
+					}
+				roll = dice1.roll();
+				System.out.println(roll);
+				if (roll == 3)
+					{
+					count++;
+					}
+			
+				System.out.println("You rolled three " + count + " times");
+			
+			}
+		
+	}
+
+
